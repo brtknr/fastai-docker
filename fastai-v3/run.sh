7bf80db7f2029fb1ddee2ca2701cc7a5b81862d9
Bharat Kunwar <b.kunwar@gmail.com>
2019-06-06 10:35:01 +0100

    Build kubeflow compatible Docker image with cuda 10.1 and Ubuntu 18.04

--- fastai-v3/run.sh
+++ fastai-v3/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source activate fastai
 ln -s /storage /notebooks/course-v3/nbs/dl1/data
-jupyter notebook --ip=0.0.0.0 --no-browser --allow-root
+jupyter notebook --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX}
